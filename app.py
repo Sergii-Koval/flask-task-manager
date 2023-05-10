5c6ee41187b6231c549ae0d60b4dabe4bfea2666
Sergii <kovalshmoval@gmail.com>
2023-05-10 17:53:53 +0200

    adding the ability to view tasks

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
+from database import db
+from models import Task
 
 app = Flask(__name__)
 app.config.from_object('config')
+db.init_app(app)
 
-db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/create-task', methods=['GET', 'POST'])
+def create_task():
+    if request.method == 'POST':
+        title = request.form['title']
+        task = Task(title=title)
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create_task.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
