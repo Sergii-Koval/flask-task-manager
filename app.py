e76cf1a0cc773b620f28b9e1de2b05686009c2e4
Sergii <kovalshmoval@gmail.com>
2023-05-10 19:11:49 +0200

    user auth

--- app.py
+++ app.py
@@ -43,6 +43,25 @@ def delete_task(id):
     db.session.commit()
     return redirect(url_for('index'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user is not None and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    if 'user_id' in session:
+        session.pop('user_id')
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -6,3 +6,7 @@ class Task(db.Model):
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=True)
     done = db.Column(db.Boolean, default=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
