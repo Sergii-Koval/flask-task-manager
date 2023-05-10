56e72b775797adb3b06ccb62c03e88c2dc329698
Sergii <kovalshmoval@gmail.com>
2023-05-10 16:45:29 +0200

    Database model setup

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 SECRET_KEY = 'a9e21b7f8d5beace6eb4ba87b3ecf0b2'
